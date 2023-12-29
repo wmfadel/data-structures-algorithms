b5127db35157c753fbee2952942eeb29dab0134d
wmfadel <fadel24997@gmail.com>
2023-12-29 17:13:37 +0200

    binary search

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	priorityqueue "data-structures-algorithms/priority_queue"
+	binarysearch "data-structures-algorithms/binary_Search"
 	"fmt"
 )
 
 func main() {
 
-	queue := priorityqueue.NewPriorityQueue[string]()
-	queue.Enqueue("B")
+	// queue := priorityqueue.NewPriorityQueue[string]()
+	// queue.Enqueue("B")
 
-	queue.Enqueue("D")
-	queue.Enqueue("C")
-	queue.Enqueue("A")
-	fmt.Println(queue.Dequeue())
-	fmt.Println(queue.Dequeue())
-	fmt.Println(queue.Dequeue())
-	fmt.Println(queue.Dequeue())
-	fmt.Println(queue.Dequeue())
+	// queue.Enqueue("D")
+	// queue.Enqueue("C")
+	// queue.Enqueue("A")
+	// fmt.Println(queue.Dequeue())
+	// fmt.Println(queue.Dequeue())
+	// fmt.Println(queue.Dequeue())
+	// fmt.Println(queue.Dequeue())
+	// fmt.Println(queue.Dequeue())
 
+	data := []int{0, 1, 2, 3, 4, 5, 6}
+
+	fmt.Println(binarysearch.Search(data, 6))
 }
